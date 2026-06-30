e7ff5e8b2f676de256d434cc9c829acc40d3f2f5
medhabarath <medha.s.barath@gmail.com>
2026-06-30 23:02:29 +0530

    remove old dataset

--- src/ccpfn/benchmarks/__init__.py
+++ src/ccpfn/benchmarks/__init__.py
@@ -2,12 +2,10 @@
 from .admit import ADMITDataset
 from .linear import SimpleLinearDataset
 from .semi_synthetic import SemiSyntheticEvalDataset, SemiSyntheticPolicyEvalDataset
-from .uhn import UHNDataset
 
 __all__ = [
     "SimpleLinearDataset",
     "ADMITDataset",
     "SemiSyntheticEvalDataset",
-    "UHNDataset",
     "SemiSyntheticPolicyEvalDataset",
 ]
