e6792cd70091444e62940da1574b8d07086793ba
chris-L6 <christopher@layer6.ai>
2026-06-30 13:25:01 +0000

    Add __init__.py

---
+++ src/ccpfn/__init__.py
@@ -0,0 +1,8 @@
+from .continuous_causal_estimator import CEPOEstimator, Prescriber
+from .evaluation import calculate_rmse
+
+__all__ = [
+    "CEPOEstimator",
+    "Prescriber",
+    "calculate_rmse",
+]
